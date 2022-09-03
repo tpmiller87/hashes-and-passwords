854db4c72b418a93e30cb4365afd3472b162a646
Tmoney$ <15959707+tpmiller87@users.noreply.github.com>
2022-09-03 17:29:55 -0400

    Update correlate.sh

--- correlate.sh
+++ correlate.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 #usage:
-#./correlate.sh <<file with hash:plaintext>> <<file with hash:user>>
-#outputs to STDOUT in format hash:plaintext:user
+# ./correlate.sh <<file with hash:plaintext>> <<file with hash:user>>
+# Outputs to STDOUT in format hash:plaintext:user, alphabetized by user.
+# Also outputs to a csv, colon (:) delimited. If
+# a plaintext password contains a colon, it will add an extra
+# column and should stand out for correction.
 
 while read line;
 do
    hash=$(echo $line | awk -F : '{print $1}'); 
    user=$(grep $hash $2 | awk -F : '{print $2}');
    echo $line:$user;
-done < $1
+done < $1 | sort -u -t ':' -k3 | sed 's/ /, /g' | tee correlated.csv
+
+echo
+echo
+echo "When viewing CSV, ensure you are delimiting by colon (:)."
